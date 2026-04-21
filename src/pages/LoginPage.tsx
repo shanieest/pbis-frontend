70ba2dd07d875a98514f2f86fc65b8d78eee8650
Jaja <160374912+shanieest@users.noreply.github.com>
2026-04-21 13:30:50 +0800

    wip

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Westfields from "../assets/Westfields.png";
 
 export default function LoginPage() {
@@ -46,6 +46,7 @@ export default function LoginPage() {
                                 type="email"
                                 id="email"
                                 className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#8B5E83] focus:border-transparent"
+                                placeholder="@westfields.edu.ph"
                                 required
                             />
                         </div>
@@ -55,10 +56,26 @@ export default function LoginPage() {
                                 type="password"
                                 id="password"
                                 className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#8B5E83] focus:border-transparent"
+                                placeholder="password"
                                 required
                             />
                         </div>
                         {error && <p className="text-red-500 text-sm">{error}</p>}
+
+                        <div className="flex items-center justify-between">
+                            <label className="flex items-center">
+                                <input
+                                    type="checkbox"
+                                    className="form-checkbox h-4 w-4 text-[#4D2B8C] focus:ring-[#8B5E83] border-gray-300 rounded"
+                                />
+                                <span className="ml-2 text-sm text-gray-700">Remember me</span>
+                            </label>
+
+                            <Link to="#" 
+                                className="text-sm text-[#4D2B8C] hover:text-[#8B5E83] transition-colors">
+                                Forgot password?
+                            </Link>
+                        </div>
                         <button
                             type="submit"
                             className="w-full py-2 px-4 bg-[#4D2B8C] text-white font-semibold rounded-lg hover:bg-[#8B5E83]"
